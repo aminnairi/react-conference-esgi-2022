50332f25cc72c162dd330d2b26a1e30bcafbafab
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-15 22:09:45 +0100

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import eslint from "vite-plugin-eslint"
 
 const port = Number(process.env.PORT)
 const host = process.env.HOST
@@ -15,7 +16,10 @@ if (!host) {
 export default defineConfig({
     root: "sources",
     plugins: [
-        react()
+        react(),
+        eslint({
+            fix: true
+        })
     ],
     server: {
         port,
