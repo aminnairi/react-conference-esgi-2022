2c656c15ee4064b1716d4ecfa61af03b2881687f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-16 11:23:08 +0100

    initial commit

--- sources/main.tsx
+++ sources/main.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router"
 import { HomePage } from "./pages/home"
+import { NotFoundPage } from "./pages/not-found"
 
 export const Main = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     )
 }
