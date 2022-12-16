12ac64e8f353ca6ac84e461409cb53600198f22f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-16 11:21:30 +0100

    initial commit

--- sources/pages/home.tsx
+++ sources/pages/home.tsx
@@ -1,5 +1,50 @@
+import { ChangeEventHandler, FormEventHandler, Fragment, useCallback, useState } from "react"
+
+interface Todo {
+    description: string
+    done: boolean
+}
+
 export const HomePage = () => {
+    const [description, setDescription] = useState("")
+    const [done, setDone] = useState(false)
+    const [todos, setTodos] = useState<Array<Todo>>([])
+
+    const updateDescription: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
+        setDescription(event.target.value)
+    }, [])
+
+    const updateDone: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
+        setDone(event.target.checked)
+    }, [])
+
+    const addTodo: FormEventHandler = useCallback((event) => {
+        event.preventDefault()
+
+        const newTodo: Todo = {
+            description,
+            done
+        }
+
+        setTodos(oldTodos => [...oldTodos, newTodo])
+        setDescription("")
+        setDone(false)
+    }, [description, done])
+
     return (
-        <h1>Home</h1>
+        <Fragment>
+            <h2>Add a todo</h2>
+            <form onSubmit={addTodo}>
+                <input type="checkbox" checked={done} onChange={updateDone} />
+                <input type="text" value={description} onChange={updateDescription} autoFocus />
+                <button type="submit">Add</button>
+            </form>
+            <h2>Todos</h2>
+            <ul>
+                {todos.map(todo => (
+                    <li>{todo.done ? "DONE" : "TODO"} {todo.description}</li>
+                ))}
+            </ul>
+        </Fragment>
     )
 }
